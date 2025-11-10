2572ff8072dc8c6e3845e0d61522a44abe7048e4
Louis Campagne <lcampagne001@iutbayonne;univ-pau.fr>
2025-11-10 11:25:01 +0100

    ajout du dossier idea et du modele favori

---
+++ modeles/favori.class.php
@@ -0,0 +1,21 @@
+<?php
+class favori {
+    private int|null $id_favori;
+    private string|null $url;
+    private string|null $image;
+    private string|null $categorie;
+    private string|null $marque;
+    private string|null $date_fav;
+    
+    public function __construct(?int $id_favori, ?string $url, ?string $image, ?string $categorie, ?string $marque, ?string $date_fav) {
+       $this->id = $id_favori;
+       $this->url = $url;
+       $this->image = $image;
+       $this->categorie = $categorie;
+       $this->marque = $marque;
+       $this->date = $date_fav;
+    }
+
+}
+
+?>
