03fbe42faec537aba148cc193d4fca4a1c591bb8
Louis Campagne <lcampagne001@iutbayonne.univ-pau.fr>
2025-11-25 15:32:36 +0100

    Ajout du controlleur utilisateur

---
+++ controllers/controller_utilisateur.class.php
@@ -0,0 +1,10 @@
+<?php
+class ControllerUtilisateur extends controller{
+    public function __construct(Twig\Environment $twig, Twig\Loader\FilesystemLoader $loader){
+        parent::__construct($twig, $loader);
+    }
+
+    public function afficher(){
+        echo "afficher favori";
+    }
+}
