1d23dc76a0c756a15a83d916623d819779240ad4
Paul Gimenez <pgimenez003@iutbayonne.univ-pau.fr>
2025-11-25 15:12:28 +0100

    Implémentation méthode afficher() dans controller article pour tester

---
+++ controllers/controller_article.class.php
@@ -0,0 +1,10 @@
+<?php
+class ControllerFavori extends controller{
+    public function __construct(Twig\Environment $twig, Twig\Loader\FilesystemLoader $loader){
+        parent::__construct($twig, $loader);
+    }
+
+    public function afficher(){
+        echo "afficher article";
+    }
+}
