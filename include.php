f4698b0f831be6d0ce0842935dfb964868568e3d
Louis Campagne <lcampagne001@iutbayonne.univ-pau.fr>
2025-11-25 09:23:10 +0100

    Ajout de la méthode getContoller dans la classe ControllerFactory

--- include.php
+++ include.php
@@ -2,6 +2,7 @@
 
 //Ajout des contrôleurs
 require_once 'controllers/controller.class.php';
+require_once 'controllers/controller_factory.class.php';
 
 //Ajout des modèles
 require_once 'modeles/article.class.php';
