0c76cb53f377b0b9e595164b50c6dac8b67ca423
Louis Campagne <lcampagne001@iutbayonne.univ-pau.fr>
2025-11-25 14:58:43 +0100

    Ajout des controlleurs à compléter et ajout de l'index

---
+++ controllers/controller_favori.class.php
@@ -0,0 +1,6 @@
+<?php
+class ControllerFavori extends controller{
+    public fonction __construct(Twig\Environment $twig, Twig\Loader\FilesystemLoader $loader){
+        parent::__construct($twig, $loader);
+    }
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//Ajout du code commun à toutes les pages
+require_once 'include.php';
+
+try {
+    
+    
+
+}catch (Exception $e) {
+   die('Erreur : ' . $e->getMessage());
+}
