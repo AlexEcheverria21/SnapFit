12ad430e44e1a5dc7df5f74eade052bef0d382c6
Louis Campagne <lcampagne001@iutbayonne.univ-pau.fr>
2025-11-25 15:06:48 +0100

    Ajout de controller à completer et de l'index

--- controllers/controller_favori.class.php
+++ controllers/controller_favori.class.php
@@ -1,6 +1,10 @@
 <?php
 class ControllerFavori extends controller{
-    public fonction __construct(Twig\Environment $twig, Twig\Loader\FilesystemLoader $loader){
+    public function __construct(Twig\Environment $twig, Twig\Loader\FilesystemLoader $loader){
         parent::__construct($twig, $loader);
+    }
+
+    public function afficher(){
+        echo "afficher favori";
     }
 }
