88d9abf82896436d69df81f01406342744c90765
Louis Campagne <lcampagne001@iutbayonne.univ-pau.fr>
2025-11-26 14:21:28 +0100

    Remplissage de l'index

--- index.php
+++ index.php
@@ -1,10 +1,38 @@
 <?php
-//Ajout du code commun à toutes les pages
+
 require_once 'include.php';
 
-try {
-    
-    
+try  {
+    if (isset($_GET['controleur'])){
+        $controllerName=$_GET['controleur'];
+    }else{
+        $controllerName='';
+    }
+
+    if (isset($_GET['methode'])){
+        $methode=$_GET['methode'];
+    }else{
+        $methode='';
+    }
+
+    //Gestion de la page d'accueil par défaut
+    if ($controllerName == '' && $methode ==''){
+        $controllerName='home'; // Adapté pour SnapFit
+        $methode='index';       // Adapté pour SnapFit
+    }
+
+    if ($controllerName == '' ){
+        throw new Exception('Le controleur n\'est pas défini');
+    }
+
+    if ($methode == '' ){
+        throw new Exception('La méthode n\'est pas définie');
+    }
+
+    // $loader et $twig viennent de config/twig.php
+    $controller = ControllerFactory::getController($controllerName, $loader, $twig);
+  
+    $controller->call($methode);
 
 }catch (Exception $e) {
    die('Erreur : ' . $e->getMessage());
