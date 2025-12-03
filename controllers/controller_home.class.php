fe5c0744647a808afc5d10fb82ea28cc6cb01db5
Clément ESTREM <clement.estrem@gmail.com>
2025-12-03 11:26:29 +0100

    Changement du nom du controller

--- controllers/controller_home.class.php
+++ controllers/controller_home.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class ControlleurHome extends Controller {
+class ControllerHome extends Controller {
 
     public function __construct(\Twig\Environment $twig, \Twig\Loader\FilesystemLoader $loader) {
         parent::__construct($twig, $loader);
