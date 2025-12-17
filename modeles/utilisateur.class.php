f53e02e596c4d2706f42370786422429299fa1bf
Alex Echeverria <alex21echeverria02@gmail.com>
2025-12-17 10:51:42 +0100

    ajout des constantes de sécurité et des attributs de sécurité dans la class utilisateur

--- modeles/utilisateur.class.php
+++ modeles/utilisateur.class.php
@@ -1,19 +1,36 @@
 <?php
+/**
+ * @file    utilisateur.class.php
+ * @author  Alex Echeverria
+ * @brief   Définit la classe Utilisateur.
+ * @version 1.0
+ * @date    17/12/2025
+ */
+
+require_once 'bd.class.php';
+
+// Constantes de sécurité
+const MAX_CONNEXIONS_ECHOUEES = 3;
+const DELAI_ATTENTE_CONNEXION = 60 * 5; // 5 minutes en secondes
 
 class Utilisateur {
 
     //Attributs privés
-    private int|null $id_utilisateur;
-    private string|null $nom;
-    private string|null $prenom;
-    private string|null $mot_de_passe_hash;
-    private string|null $role;
-    private string|null $date_inscription;
-    private string|null $email;
-    private string|null $nom_connexion;
-    private string|null $sexe;
-    private string|null $pays;
+    private ?int $id_utilisateur = null;
+    private ?string $nom = null;
+    private ?string $prenom = null;
+    private ?string $email; // Obligatoire
+    private ?string $mot_de_passe_hash; // Obligatoire
+    private string $role = 'user';
+    private ?string $nom_connexion = null;
+    private ?string $date_inscription = null;
+    private ?string $sexe = null;
+    private ?string $pays = null;
 
+    // Propriétés de Sécurité
+    private int $tentativesEchouees = 0;
+    private ?string $dateDernierEchecConnexion = null;
+    private string $statutCompte = 'actif';
 
     //Contructeur
     public function __construct(?int $id_utilisateur = null, ?string $nom = null, ?string $prenom = null, ?string $mot_de_passe_hash = null, ?string $role = null, ?string $date_inscription = null, ?string $email = null, ?string $nom_connexion = null, ?string $sexe = null, ?string $pays = null ) {
